4f589b5f848be7e6ca41aae95f830d451b12908b
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-07-30 15:56:39 -0400

    update version

--- src/main/java/com/ocraftyone/rebindnarrator/mixin/KeyBindingMixin.java
+++ src/main/java/com/ocraftyone/rebindnarrator/mixin/KeyBindingMixin.java
@@ -5,9 +5,6 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyBinding.class)
 public abstract class KeyBindingMixin implements KeyBindingModifiedAccessor {
